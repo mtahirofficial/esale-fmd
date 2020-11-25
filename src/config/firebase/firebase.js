7af63e5b2581f70c949d436400d6e1eaf907f525
mtahirofficial <hmtahirs1@gmail.com>
2020-11-25 20:07:02 +0500

    Extra cde is removed

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -15,16 +15,3 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export default firebase
-
-
-
-// export default firebase.initializeApp({
-//   apiKey: "AIzaSyDj6ZbPFWIQlVimq37FmO_mI6-GiovSQZs",
-//   authDomain: "esale-fmd.firebaseapp.com",
-//   databaseURL: "https://esale-fmd.firebaseio.com",
-//   projectId: "esale-fmd",
-//   storageBucket: "esale-fmd.appspot.com",
-//   messagingSenderId: "505056774229",
-//   appId: "1:505056774229:web:bc19d4ca12cc0eaf11ecdc",
-//   measurementId: "G-JGVBC2BZNT"
-// });
