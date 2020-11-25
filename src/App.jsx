eb21df167fa050c9f70818e048125568055a77b1
mtahirofficial <hmtahirs1@gmail.com>
2020-11-25 20:06:10 +0500

    'FirebaseAuthProvider' component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import './App.css';
-import Login from './containers/Login';
-
+import "./App.css";
+import Router from "./config/router/Router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faHeart, far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import firebase from "./config/firebase/firebase";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+library.add(fab, far, fas, faHeart);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
-    </div>
+    <FirebaseAuthProvider firebase={firebase}>
+      <Router />
+    </FirebaseAuthProvider>
   );
 }
-
 export default App;
