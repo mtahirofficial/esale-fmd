139ece5c38480d0eea18835dc008dcff0a517770
mtahirofficial <hmtahirs1@gmail.com>
2020-12-11 23:56:04 +0500

    FirebaseAuthProvider Removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faHeart, far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import firebase from "./config/firebase/firebase";
-import { FirebaseAuthProvider } from "@react-firebase/auth";
 library.add(fab, far, fas, faHeart);
 function App() {
   return (
-    <FirebaseAuthProvider firebase={firebase}>
       <Router />
-    </FirebaseAuthProvider>
   );
 }
 export default App;
