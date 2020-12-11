80e8f59251b9b197facc574ec2ddef71d9f80110
mtahirofficial <hmtahirs1@gmail.com>
2020-12-11 23:59:11 +0500

    Login Condition Removed

--- src/config/router/Router.js
+++ src/config/router/Router.js
@@ -1,29 +1,22 @@
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
-import Home from '../../containers/Home';
-import Login from '../../containers/Login';
-import Header from '../../containers/Header';
-import { connect } from 'react-redux'
+import Home from "../../containers/Home";
+import Login from "../../containers/Login";
+import Header from "../../containers/Header";
+import { connect } from "react-redux";
+import CategoriesList from "../../components/CategoriesList";
+import Post from "../../components/Post";
 const Router = (props) => {
-    // console.log(props.user)
-    let token;
-    if(props.user.length > 0){
-        token = props.user[0].token
-    } else {
-        console.log(props.user)
-    }
-    return(
-        <BrowserRouter>
-            <Header />
-            <Route exact path='/' render = {() => {
-                token ? <Home /> : <Redirect to='/login' />
-            }} />
-            <Route exact path='/login' component={Login} />
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Header />
+      <CategoriesList />
+      <Route exact path="/post" render={Post} />
+    </BrowserRouter>
+  );
+};
 
 const mapStateToProps = (store) => ({
-    user: store
-})
+  user: store,
+});
 
-export default connect(mapStateToProps, null)(Router)
+export default connect(mapStateToProps, null)(Router);
