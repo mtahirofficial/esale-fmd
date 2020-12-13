fc1e7f62d4c5b02374b97b3a37d8d42437628cd1
mtahirofficial <hmtahirs1@gmail.com>
2020-12-13 17:25:48 +0500

    React import

--- src/config/router/Router.js
+++ src/config/router/Router.js
@@ -1,16 +1,17 @@
+import React from 'react'
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import Home from "../../containers/Home";
 import Login from "../../containers/Login";
 import Header from "../../containers/Header";
 import { connect } from "react-redux";
 import CategoriesList from "../../components/CategoriesList";
-import Post from "../../components/Post";
+import AddPost from '../../components/AddPost';
 const Router = (props) => {
   return (
     <BrowserRouter>
       <Header />
       <CategoriesList />
-      <Route exact path="/post" render={Post} />
+      <Route exact path="/post" component={AddPost} />
     </BrowserRouter>
   );
 };
