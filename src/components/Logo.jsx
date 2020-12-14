666a3808f68b862a2976066ce8742012bb644b76
mtahirofficial <hmtahirs1@gmail.com>
2020-12-14 21:40:42 +0500

    Logo is Designed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,55 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import "../static/css/fonts.css";
 const Logo = (props) => {
   const cls1 = {
-    fill: "#00acda",
+    fontSize: "180.69px",
+    fill: "#148496",
   };
   const cls2 = {
-    fontSize: "142.25px",
-    fill: "#252525",
-    fontFamily: "'Exotic350BT-DemiBold', 'Exotc350 DmBd BT'",
-    fontWeight: "300",
+    fontSize: "180.69px",
+    fill: "#000",
   };
   const cls3 = {
-    letterSpacing: "0em",
+    fill: "#none",
+    strokeMiterlimit: "10",
+    strokeWidth: "10px",
+    stroke: "#000",
   };
+  const cls4 = {
+    fill: "none",
+    strokeMiterlimit: "10",
+    strokeWidth: "10px",
+    stroke: "#148496",
+  };
+
   return (
-    <div style={{ display: "inline-block" }}>
-      <Link to={props.to}>
-        <svg
-          style={{ padding: "10px" }}
-          height={props.height}
-          width={props.width}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 788 567"
-        >
-          <title>Logo - fm devndesign</title>
-          <g id="Layer_5" data-name="Layer 5">
-            <path
-              style={cls1}
-              d="M88,175.63l161-96s20-13,27,4v51s3,13-10,18c-15,7-109,60-109,60l105,59,8,5s6,1,6,11v53s-2,14-22,5c-24-13-194-111-194-111a10.7,10.7,0,0,1-8-12v-21s-1-4,5-8S88,175.63,88,175.63Z"
-            />
-            <path
-              style={cls1}
-              d="M700,175.63l-161-96s-20-13-27,4v51s-3,13,10,18c15,7,109,60,109,60l-105,59-8,5s-6,1-6,11v53s2,14,22,5c24-13,194-111,194-111a10.7,10.7,0,0,0,8-12v-21s1-4-5-8S700,175.63,700,175.63Z"
-            />
-            <path
-              style={cls1}
-              d="M390,142.31s60-106,67-113c2-5,5-13,24-9,17,9,16,12,14,21s-20,78-24,89c-15,64-43,130-77,171,0,0,12,47-17,69-10,7-16,17-95,34-8-1-22-4-6-18,8-7,22-14,24-33,1-13-7-55,40-70C339,255.31,365,180.31,390,142.31Z"
-            />
-            <text style={cls2} transform="translate(19 514.09)">
-              fm dev
-              <tspan style={cls1} x="344.86" y="0">
-                n
-              </tspan>
-              <tspan style={cls3} x="409.59" y="0">
-                design
-              </tspan>
-            </text>
-          </g>
-        </svg>
-      </Link>
+    <div style={{ display: "inline-block", padding: "0px" }}>
+      <svg
+        height={props.height}
+        width={props.width}
+        id="Logo"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 941.47 236.55"
+      >
+        <title>ebazaar</title>
+        <text style={cls1} transform="translate(341.43 163.76)">
+          E
+          <tspan style={cls2} x="88.49" y="0">
+            bazaar
+          </tspan>
+        </text>
+        <line style={cls3} x1="316.81" y1="50.4" x2="175.11" y2="50.4" />
+        <line style={cls3} x1="305.44" y1="109.14" x2="147.6" y2="109.14" />
+        <line style={cls3} x1="311.3" y1="79.77" x2="161.19" y2="79.77" />
+        <path
+          style={cls4}
+          d="M331.1,41S316,133.07,310.43,151.79,294.74,168.24,221,166.55c-39.62-.92-77.49,6.34-79.09-34.52-1.13-28.75,6.5-82.75-34-96.74-25.46-8.79-96.07-18-96.07-18"
+          transform="translate(-11.15 -12.36)"
+        />
+        <path
+          style={cls4}
+          d="M144.38,200.83l142.32-.15c0-.62,51,4.89,47.05-18.78-2.65-16.86-38.13-14.43-50.81-14.43"
+          transform="translate(-11.15 -12.36)"
+        />
+        <circle style={cls4} cx="298.97" cy="216.24" r="15.31" />
+        <circle style={cls4} cx="151.17" cy="216.24" r="15.31" />
+      </svg>
     </div>
   );
 };
